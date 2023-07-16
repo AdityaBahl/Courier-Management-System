4ab41edf6a8c759de0aebc2505770a9d80ab61a2
Aditya Bahl <adityabahl12345@gmail.com>
2023-07-16 12:32:50 +0530

    PDF Viewer

--- src/components/PDFViewer.js
+++ src/components/PDFViewer.js
@@ -1,11 +1,17 @@
 import React from 'react';
-import { Document, Page } from 'react-pdf';
-const PDFViewer = ({ pdfUrl }) => {
+import { Worker, Viewer } from '@react-pdf-viewer/core';
+import '@react-pdf-viewer/core/lib/styles/index.css';
+import { pdfjs } from 'react-pdf';
+import myPDF from "../pages/Report.pdf"
+import '../styles/PDFviewer.css'
+import Navbar from '../components/Navbar'
+const PDFViewer = () => {
     return (
         <div>
-            <Document file={pdfUrl}>
-                <Page pageNumber={1} />
-            </Document>
+            <Navbar />
+            <object data="https://drive.google.com/file/d/1v7geIPrEDdAIsJVph49vN_0QvvD0gS2-/view?usp=drive_link" type="application/pdf" width="100%" height="100%">
+                <p>Alternative text - include a link <a href="http://africau.edu/images/default/sample.pdf">to the PDF!</a></p>
+            </object>
         </div>
     );
 };
